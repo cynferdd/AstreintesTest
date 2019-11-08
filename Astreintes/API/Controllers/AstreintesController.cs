ae25903f4e8ee72b71f966d9e445daa1277b2094
Mathieu JOLIVET <m.jolivet@linkbynet.com>
2019-11-08 13:13:44 +0100

    Get and Get/{Id} api routes

--- Astreintes/API/Controllers/AstreintesController.cs
+++ Astreintes/API/Controllers/AstreintesController.cs
@@ -28,11 +28,56 @@ namespace API.Controllers
         }
         #endregion
 
+        /// <summary>
+        /// retrieving all slots, or filtering them with a period
+        /// </summary>
+        /// <param name="begin">Begin date</param>
+        /// <param name="end">End date</param>
+        /// <returns>list of duty slots</returns>
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery]DateTime? begin, [FromQuery]DateTime? end)
         {
+            IEnumerable<Slot> slots;
+            if (begin.HasValue || end.HasValue)
+            {
+                slots = await slotService.GetByPeriod(begin == null ? DateTime.MinValue : begin.Value,end == null ? DateTime.MaxValue : end.Value).ConfigureAwait(false);
+            }
+            else
+            {
+                slots = await slotService.GetAll().ConfigureAwait(false);
+            }
+            
+            if (slots == null || !slots.Any())
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(slots);
+            }
 
-            IEnumerable<Slot> slots = await slotService.GetAll().ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Get duty slots by assignee id
+        /// </summary>
+        /// <param name="id">assignee id</param>
+        /// <param name="begin">filtering begin date</param>
+        /// <param name="end">filtering end date</param>
+        /// <returns>list of slots</returns>
+        [HttpGet("id")]
+        public async Task<IActionResult> Get(int id, [FromQuery]DateTime? begin, [FromQuery]DateTime? end)
+        {
+            IEnumerable<Slot> slots;
+            if (begin.HasValue || end.HasValue)
+            {
+                slots = await slotService.GetByAssigneeIdAndPeriod(id, begin == null ? DateTime.MinValue : begin.Value, end == null ? DateTime.MaxValue : end.Value).ConfigureAwait(false);
+            }
+            else
+            {
+                slots = await slotService.GetByAssigneeId(id).ConfigureAwait(false);
+            }
+
             if (slots == null || !slots.Any())
             {
                 return NotFound();
